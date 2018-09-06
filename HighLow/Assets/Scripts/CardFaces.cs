8860261d40bb8db814a6e07a3c6c24ac3e0930df
Raymond Liang <rliang1994@gmail.com>
2018-09-06 02:11:41 -0700

    added card comparisons

---
+++ HighLow/Assets/Scripts/CardFaces.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardFaces : MonoBehaviour {
+
+    public Sprite[] faces;
+    public int[] indexArray = new int[52];
+    SpriteRenderer cardsprite;
+    public GameObject cardBack;
+    public GameObject cardFront;
+    int CardIndex;
+    public int value { get; set; }
+    public int suit { get; set; }
+    public bool isFlipped = false;
+
+    void Start()
+    {
+        for (int i = 0; i < indexArray.Length; i++)
+        {
+            indexArray[i] = i;
+        }
+        Shuffle(indexArray);
+        cardBack.SetActive(true);
+        cardFront.SetActive(false);
+        cardsprite = cardFront.gameObject.GetComponent<SpriteRenderer>();
+        CardIndex = getShuffledCard();
+        setCardInfo(CardIndex);
+
+        //Debug.Log("value: " + value + " suit: " + suit);
+        cardsprite.sprite = faces[CardIndex];
+    }
+
+    public void setCardSprite(int CardIndex)
+    {
+        cardsprite = cardFront.gameObject.GetComponent<SpriteRenderer>();
+        cardsprite.sprite = faces[CardIndex];
+    }
+
+    public void setCardInfo(int CardIndex)
+    {
+        if (CardIndex < 13)
+        {
+            value = CardIndex + 1;
+            suit = 2;
+        }
+        else if (CardIndex >= 13 && CardIndex < 26)
+        {
+            value = CardIndex + 1 - 13;
+            suit = 1;
+        }
+        else if (CardIndex >= 26 && CardIndex < 39)
+        {
+            value = CardIndex + 1 - 26;
+            suit = 3;
+        }
+        else
+        {
+            value = CardIndex + 1 - 39;
+            suit = 4;
+        }
+    }
+
+    public int getRandomCard()
+    {
+        return Random.Range((int)0, (int)51);
+    }
+
+    public int getShuffledCard()
+    {
+        int index = getRandomCard();
+        return indexArray[index];
+    }
+
+
+    void flipCard()
+    {
+        cardBack.SetActive(false);
+        cardFront.SetActive(true);
+        isFlipped = true;
+    }
+
+    void OnMouseDown()
+    {
+        flipCard();
+    }
+
+    public void Shuffle(int[] ints)
+    {
+        int temp;
+        for (int i = 0; i < indexArray.Length; i++)
+        {
+            temp = ints[i];
+            int swapIndex = getRandomCard();
+            ints[i] = ints[swapIndex];
+            ints[swapIndex] = temp;
+        }
+    }
+}
---
+++ HighLow/Assets/Scripts/DeckManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeckManager : MonoBehaviour {
+
+    public GameObject CardPrefab;
+    public GameObject CardPosition1;
+    public GameObject CardPosition2;
+    int FirstCardValue, SecondCardValue, FirstCardSuit, SecondCardSuit;
+    private Sprite cardsprite1, cardsprite2;
+    public GameObject card1, card2;
+    public Text ValueText, SuitText;
+
+
+    void placeCards()
+    {
+        card1.transform.position = CardPosition1.transform.position;
+        card2.transform.position = CardPosition2.transform.position;
+    }
+
+	// Use this for initialization
+	void Start () {
+        ValueText.enabled = false;
+        SuitText.enabled = false;
+        placeCards();
+        checkDuplicateCard();
+	}
+
+    void Update()
+    {
+        if (card1.gameObject.GetComponent<CardFaces>().isFlipped && card2.gameObject.GetComponent<CardFaces>().isFlipped)
+        {
+            checkHighorLow();
+        }
+    }
+
+    void checkDuplicateCard()
+    {
+        FirstCardValue = card1.gameObject.GetComponent<CardFaces>().value;
+        FirstCardSuit = card1.gameObject.GetComponent<CardFaces>().suit;
+
+        SecondCardValue = card2.gameObject.GetComponent<CardFaces>().value;
+        SecondCardSuit = card2.gameObject.GetComponent<CardFaces>().suit;
+
+        while((FirstCardValue==SecondCardValue) && (FirstCardSuit == SecondCardSuit))
+        {
+            int RandomCardIndex = card2.gameObject.GetComponent<CardFaces>().getShuffledCard();
+            card2.gameObject.GetComponent<CardFaces>().setCardInfo(RandomCardIndex);
+            card2.gameObject.GetComponent<CardFaces>().setCardSprite(RandomCardIndex);
+            SecondCardValue = card2.gameObject.GetComponent<CardFaces>().value;
+            SecondCardSuit = card2.gameObject.GetComponent<CardFaces>().suit;
+        }
+    }
+
+    void checkHighorLow()
+    {
+        ValueText.enabled = true;
+        SuitText.enabled = true;
+
+        FirstCardValue = card1.gameObject.GetComponent<CardFaces>().value;
+        FirstCardSuit = card1.gameObject.GetComponent<CardFaces>().suit;
+
+        SecondCardValue = card2.gameObject.GetComponent<CardFaces>().value;
+        SecondCardSuit = card2.gameObject.GetComponent<CardFaces>().suit;
+
+        if (SecondCardValue > FirstCardValue)
+        {
+            ValueText.text = "Higher Value ";
+        }
+        else if (SecondCardValue < FirstCardValue)
+        {
+            ValueText.text = "Lower Value ";
+        }
+        else
+        {
+            ValueText.text = "Same Value ";
+        }
+
+        if (SecondCardSuit > FirstCardSuit)
+        {
+            SuitText.text = "Higher Suit";
+        }
+        else if (SecondCardSuit < FirstCardSuit)
+        {
+            SuitText.text = "Lower Suit";
+        }
+        else
+        {
+            SuitText.text = "Same Suit";
+        }
+    }
+
+}
